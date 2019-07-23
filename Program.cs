3588596cc9084b9219ec3363b0413a64cae8a19c
gugu <gustavocorrea2032@gmail.com>
2019-07-23 12:37:03 -0300

    CRSA

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace FunçãoCRSA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            StreamWriter arquivo = new StreamWriter(@"c:\Gugu\clientes.rtf",true);
+            Console.WriteLine("Digite o seu nome: ");
+            nome = Console.ReadLine();
+
+            arquivo.WriteLine("Nome: "+nome+"- Data Cadastro: "+data());
+            arquivo.Close();
+        }
+
+        /// <summary>
+        /// A função data é responsável por pegar a data do sistema.
+        /// </summary>
+        /// <returns>Retorna a data do sistema</returns>
+        static string data(){
+            return DateTime.Now.ToString();
+
+        
+        }
+
+        
+
+    }
+}
